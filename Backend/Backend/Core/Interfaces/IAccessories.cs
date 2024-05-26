2af03f53dddf0d06c3c465cad2c61858aba94916
Diella <152415294+dielladiella@users.noreply.github.com>
2024-05-26 22:53:57 +0200

    Add files via upload

---
+++ Backend/Backend/Core/Interfaces/IAccessories.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend.Core.Dtos.Accessory;
+
+namespace backend.Core.Interfaces
+{
+    public interface IAccessoryService
+    {
+        Task<IEnumerable<AccessoryDto>> GetAllAccessoriesAsync();
+        Task<AccessoryDto> GetAccessoryByIdAsync(int id);
+        Task<AccessoryDto> AddAccessoryAsync(AccessoryDto accessoryDto);
+        Task<AccessoryDto> UpdateAccessoryAsync(int id, AccessoryDto accessoryDto);
+        Task DeleteAccessoryAsync(int id);
+    }
+}
